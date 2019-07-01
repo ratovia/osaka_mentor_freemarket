29926be0cf182d8e1356ee93af547763aaf7f185
ratovia <ratovia@hotmail.co.jp>
2019-07-01 14:14:34 +0900

    add sign_in routing and controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,11 +3,19 @@ class WelcomeController < ApplicationController
   end
   def mypage
   end
+  def identification
+    @prefecture = Prefecture.all
+  end
   def new
   end
   def new2
   end
-  def identification
-    @prefecture = Prefecture.all
+  def new3
+  end
+  def new4
+  end
+  def new5
+  end
+  def new6
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
   get 'welcome/mypage' => 'welcome#mypage'
   get 'welcome/new' => 'welcome#new'
   get 'welcome/new2' => 'welcome#new2'
+  get 'welcome/new3' => 'welcome#new3'
+  get 'welcome/new4' => 'welcome#new4'
+  get 'welcome/new5' => 'welcome#new5'
+  get 'welcome/new6' => 'welcome#new6'
   get 'welcome/identification' => 'welcome#identification'
 end
