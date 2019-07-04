f27edc287ed9a2d3b48f97c68b857fd86b3759de
hidemaro nakano <vita.hima-hima-0427@ezweb.ne.jp>
2019-07-04 13:04:24 +0900

    add login_page

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,5 @@ Rails.application.routes.draw do
   get 'welcome/identification' => 'welcome#identification'
   get 'welcome/new_item' => 'welcome#new_item'
   get 'welcome/finish' => 'welcome#finish'
+  get 'welcome/login_page' => 'welcome#login_page'
 end
