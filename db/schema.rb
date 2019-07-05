ae541f366ce0a794492ac274abc81bad0dd55c3c
hidemaro nakano <vita.hima-hima-0427@ezweb.ne.jp>
2019-07-05 16:41:23 +0900

    add user db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_26_072559) do
+ActiveRecord::Schema.define(version: 2019_07_05_072519) do
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "nickname", null: false
+    t.string "first_name", null: false
+    t.string "last_name", null: false
+    t.string "first_name_kana", null: false
+    t.string "last_name_kana", null: false
+    t.integer "birthday", null: false
+    t.string "image", null: false
+    t.integer "postal_cade", null: false
+    t.string "prefecture", null: false
+    t.string "city", null: false
+    t.integer "address_num", null: false
+    t.string "building", null: false
+    t.integer "tell", null: false
+    t.string "gender", null: false
+    t.text "profile", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
 
   create_table "welcomes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", null: false
