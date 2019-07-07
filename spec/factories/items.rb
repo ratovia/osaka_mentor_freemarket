263f9777e2c86eb35c25c200fee5c49988fc6983
hidemaro nakano <vita.hima-hima-0427@ezweb.ne.jp>
2019-07-07 20:08:02 +0900

    add test_item

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :item do
-    
+    name {"testitem"}
+    description {"testtesttesttesttest"}
+    status {"新品"}
+    delivery_burden {"送料込み(出品者負担)"}
+    delivery_method {"らくらくメルカリ便"}
+    delivery_prefecture{"1"}
+    delivery_time {"3~4日"}
+    price {"100000"}
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,70 @@
 require 'rails_helper'
 
-RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe "Item" do
+  before do
+    @item = create(:item)
+  end
+  
+  describe "invalid" do
+    it "商品名が空の時出品できない" do
+      item = Item.new(
+        name: "",
+        description: "iiiiiiiiiiiiiiiii",
+        status: "新品",
+        delivery_burden: "送料込み(出品者負担)",
+        delivery_method: "らくらくメルカリ便",
+        delivery_prefecture: "1",
+        delivery_time: "3~4日",
+        price: "100000"
+      )
+      item.valid?
+      expect(item.errors[:name]).to include("can't be blank")
+    end
+
+    it "価格が空の時出品できない" do
+      item = Item.new(
+        name: "アイテム",
+        description: "iiiiiiiiiiiiiiiii",
+        status: "新品",
+        delivery_burden: "送料込み(出品者負担)",
+        delivery_method: "らくらくメルカリ便",
+        delivery_prefecture: "1",
+        delivery_time: "3~4日",
+        price: ""
+      )
+      item.valid?
+      expect(item.errors[:price]).to include("can't be blank")
+    end
+
+    it "商品の状態が空の時出品できない" do
+      item = Item.new(
+        name: "アイテム",
+        description: "iiiiiiiiiiiiiiiii",
+        status: "",
+        delivery_burden: "送料込み(出品者負担)",
+        delivery_method: "らくらくメルカリ便",
+        delivery_prefecture: "1",
+        delivery_time: "3~4日",
+        price: "100000"
+      )
+      item.valid?
+      expect(item.errors[:status]).to include("can't be blank")
+    end
+
+    it "商品名が41文字以上の時に出品できない" do
+      item = Item.new(
+        name: "アイテム" * 41,
+        description: "iiiiiiiiiiiiiiiii",
+        status: "新品",
+        delivery_burden: "送料込み(出品者負担)",
+        delivery_method: "らくらくメルカリ便",
+        delivery_prefecture: "1",
+        delivery_time: "3~4日",
+        price: "100000"
+      )
+      item.valid?
+      expect(item.errors[:name]).to include("is too long (maximum is 40 characters)")
+    end
+  end
 end
+
