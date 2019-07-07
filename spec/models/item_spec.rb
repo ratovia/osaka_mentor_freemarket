c1beccf0d494fa6fb729de1ec0d89232eb1e734c
hidemaro nakano <vita.hima-hima-0427@ezweb.ne.jp>
2019-07-07 20:55:10 +0900

    add test description thousand or more error

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -9,7 +9,7 @@ describe "Item" do
     it "商品名が空の時出品できない" do
       item = Item.new(
         name: "",
-        description: "iiiiiiiiiiiiiiiii",
+        description: "商品説明欄",
         status: "新品",
         delivery_burden: "送料込み(出品者負担)",
         delivery_method: "らくらくメルカリ便",
@@ -24,7 +24,7 @@ describe "Item" do
     it "価格が空の時出品できない" do
       item = Item.new(
         name: "アイテム",
-        description: "iiiiiiiiiiiiiiiii",
+        description: "商品説明欄",
         status: "新品",
         delivery_burden: "送料込み(出品者負担)",
         delivery_method: "らくらくメルカリ便",
@@ -39,7 +39,7 @@ describe "Item" do
     it "商品の状態が空の時出品できない" do
       item = Item.new(
         name: "アイテム",
-        description: "iiiiiiiiiiiiiiiii",
+        description: "商品説明欄",
         status: "",
         delivery_burden: "送料込み(出品者負担)",
         delivery_method: "らくらくメルカリ便",
@@ -54,7 +54,7 @@ describe "Item" do
     it "商品名が41文字以上の時に出品できない" do
       item = Item.new(
         name: "アイテム" * 41,
-        description: "iiiiiiiiiiiiiiiii",
+        description: "商品説明欄",
         status: "新品",
         delivery_burden: "送料込み(出品者負担)",
         delivery_method: "らくらくメルカリ便",
@@ -65,6 +65,20 @@ describe "Item" do
       item.valid?
       expect(item.errors[:name]).to include("is too long (maximum is 40 characters)")
     end
+
+    it "商品説明が1000文字以上の時に出品できない" do
+      item = Item.new(
+        name: "アイテム",
+        description: "商品説明欄" * 201,
+        status: "新品",
+        delivery_burden: "送料込み(出品者負担)",
+        delivery_method: "らくらくメルカリ便",
+        delivery_prefecture: "1",
+        delivery_time: "3~4日",
+        price: "100000"
+      )
+      item.valid?
+      expect(item.errors[:description]).to include("is too long (maximum is 1000 characters)")
+    end
   end
 end
-
