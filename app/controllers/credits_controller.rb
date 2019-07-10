868c844a109140e49105806cab6a302fb612268b
ratovia <ratovia@hotmail.co.jp>
2019-07-10 19:10:46 +0900

    add create action

--- app/controllers/credits_controller.rb
+++ app/controllers/credits_controller.rb
@@ -15,5 +15,21 @@ class CreditsController < ApplicationController
       redirect_to credits_path
     end
   end
+
+  def create
+    unless current_user.credits.blank?
+      credit = current_user.credits.first
+      @credit = create_payjp_card(current_user , credit.costomer)
+    else
+      customer = create_payjp_customer()
+      @credit = create_payjp_card(current_user, customer)
+    end
+    
+    if @credit.save
+      redirect_to credits_path
+    else
+      render "new"
+    end
+  end
   end
 end
