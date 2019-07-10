c8ea0b0f96bcebd5c56c876c175db535ec494d98
ratovia <ratovia@hotmail.co.jp>
2019-07-10 18:51:09 +0900

    add credits routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'welcome#index'
-  resources :items, only: [:index,:new,:create,:show]
-  resources :credits, only: [:index, :new]
+  root 'items#index'
+  resources :items, only: [:index, :new, :create, :show]
+  resources :credits, only: [:index, :new, :create, :destroy]
 end
