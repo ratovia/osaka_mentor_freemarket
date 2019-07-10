ca8f4a8dfba787d6132b11549b31c288557a6f61
hidemaro nakano <vita.hima-hima-0427@ezweb.ne.jp>
2019-07-10 18:49:13 +0900

    add: id DESC test

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -7,4 +7,11 @@ describe ItemsController do
       expect(response).to render_template :index
     end
   end
+  describe 'GET #index' do
+    it "出品したidが降順に並び変えられているか" do
+      items = create_list(:item, 4)
+      get :index
+      expect(assigns(:items)).to match(items.sort{ |a, b| b.id <=> a.id })
+    end
+  end
 end
