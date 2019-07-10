cf1a950787a47d774b92eb91f90e278095f92c88
ratovia <ratovia@hotmail.co.jp>
2019-07-10 19:09:35 +0900

    add index action

--- app/controllers/credits_controller.rb
+++ app/controllers/credits_controller.rb
@@ -1,7 +1,14 @@
 class CreditsController < ApplicationController
   def index
+    unless current_user.credits.blank?
+      Payjp.api_key = get_payjp_key()
+      credit = current_user.credits.first
+      customer = Payjp::Customer.retrieve(credit.customer_id)
+      card = customer.cards.retrieve(customer.default_card)
+      @card_info = get_card_info(card)
   end
 
+  end
   def new
   end
 end
