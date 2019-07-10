8b0622a76a3ae5126c4de18d34d804e5508bb414
maeda5141 <maedak514@gmail.com>
2019-07-10 17:33:02 +0900

    update_item_show_page

--- app/assets/javascripts/show_item.js
+++ app/assets/javascripts/show_item.js
@@ -6,8 +6,11 @@ $(function () {
     $(this).css('opacity', '1');
   });
   var price = $('.main_price__display').text();
-  $('.main_price__displa').text('¥' + String(price).replace(/\B(?=(\d{3})+(?!\d))/g, ','));
-console.log(price);
-
+  $('.new_item__text_area--price_display').each(function (index, element) {
+    var itemPrice = $(element).text();
+    $(element).text('¥' + String(itemPrice).replace(/\B(?=(\d{3})+(?!\d))/g, ','));
+  })
+  
+  $('.main_price__display').text('¥' + String(price).replace(/\B(?=(\d{3})+(?!\d))/g, ','));
 
 });
