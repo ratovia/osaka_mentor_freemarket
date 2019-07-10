ccac0d340412b47368ef7f3973e861ddd74719a6
ratovia <ratovia@hotmail.co.jp>
2019-07-10 19:11:14 +0900

    update  credits  controller

--- app/controllers/credits_controller.rb
+++ app/controllers/credits_controller.rb
@@ -6,8 +6,8 @@ class CreditsController < ApplicationController
       customer = Payjp::Customer.retrieve(credit.customer_id)
       card = customer.cards.retrieve(customer.default_card)
       @card_info = get_card_info(card)
-  end
-
+    end
+    
   end
   
   def new
@@ -31,5 +31,56 @@ class CreditsController < ApplicationController
       render "new"
     end
   end
+
+  def destroy 
+    unless current_user.credits.blank?
+      Payjp.api_key = get_payjp_key()
+      credit = current_user.credits.first
+      customer = Payjp::Customer.retrieve(credit.customer_id)
+      customer.delete
+      credit.destroy
+    end
+    redirect_to action: "index"
   end
+
+  private
+
+  def create_payjp_customer
+    Payjp.api_key = get_payjp_key()
+    customer = Payjp::Customer.create()
+  end
+
+  def create_payjp_card(user,customer)
+    customer.cards.create(card: token_parmas[:token])
+    credit = Credit.new(
+      user_id: user.id,
+      customer_id: customer.id,
+    )
+  end
+
+  def token_parmas
+    params.permit(:token)
+  end
+
+  def get_payjp_key
+    ENV["MERUCARI_PAYJP_SECRET_KEY"]
+  end
+
+  def get_card_info(card)
+    card_info = {}
+    if card.brand == "Visa"
+      card_info[:brand] = "visa.svg"
+    elsif card.brand == "master"
+      card_info[:brand] = "master.png"
+    elsif card.brand == "jcb"
+      card_info[:brand] = "jcb.png"
+    else
+      card_info[:brand] = "non-card.png"
+    end
+    
+    card_info[:last4] = "*" * 12 + card.last4
+    card_info[:exp_date] = "#{card.exp_month}/#{card.exp_year.to_s.slice(2,2)}"
+    return card_info
+  end
+
 end
