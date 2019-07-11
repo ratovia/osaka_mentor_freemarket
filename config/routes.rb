4f0650a7d6e76e28cbb7fbc3ec06d23e03fff45d
ratovia <ratovia@hotmail.co.jp>
2019-07-11 17:26:04 +0900

    add buy_history to routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'items#index'
-  resources :items, only: [:index, :new, :create, :show]
+  resources :items, only: [:index, :new, :create, :show] do
+    resources :buy_histories, only: [:new, :create]
+  end
   resources :credits, only: [:index, :new, :create, :destroy]
 end
