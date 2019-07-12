3c3becf70f5472ffdbd8b692f8ad29ce4f122bd9
ratovia <ratovia@hotmail.co.jp>
2019-07-12 23:59:41 +0900

    add authenticate_user!

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: [:index,:show]
   before_action :set_item, except: [:index,:create,:new]
   def index
     @items = Item.limit(4).order("id DESC")
