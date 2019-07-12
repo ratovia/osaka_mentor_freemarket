03812cd919fc38c79029c4a2c120b346fe66cda3
hidemaro nakano <vita.hima-hima-0427@ezweb.ne.jp>
2019-07-12 14:28:55 +0900

    add: sns authentication unimplemented

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users,
+  controllers: {
+  registrations: 'users/registrations' ,
+  omniauth_callbacks: 'users/omniauth_callbacks'
+  }
+  # devise_for :users
+  # devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root 'items#index'
   resources :items, only: [:index,:new,:create,:show]
+  resources :users, only: [:new]
 end
