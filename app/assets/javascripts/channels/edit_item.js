253d278863e0ed601dfb951b89c134349620364f
maeda5141 <maedak514@gmail.com>
2019-07-12 21:02:07 +0900

    edit_page_fee_and_benefit

--- app/assets/javascripts/channels/edit_item.js
+++ app/assets/javascripts/channels/edit_item.js
@@ -4,5 +4,10 @@ $(function () {
     if (!confirm) {
       return false;
     }
-  })
+  });
+  var price = $('.edit_item').val();
+  var salesFee = Math.floor(price / 10);
+  var salesBenefit = price - salesFee;
+  $('.edit_page_fee').text('¥' + String(salesFee).replace(/\B(?=(\d{3})+(?!\d))/g, ','));
+  $('.edit_page_benefit').text('¥' + String(salesBenefit).replace(/\B(?=(\d{3})+(?!\d))/g, ','));
 });
