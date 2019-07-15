87e2f8ff7548ddd676f16bde97a23ad05e0247b4
hidemaro nakano <vita.hima-hima-0427@ezweb.ne.jp>
2019-07-15 15:03:22 +0900

    add: credit

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -3,7 +3,7 @@ crumb :root do
 end
 
 crumb :user do
-  link "マイページ", user_path
+  link "マイページ", user_path(current_user)
   parent :root
 end
 
@@ -11,23 +11,8 @@ crumb :logout do
   link "ログアウト"
   parent :user
 end
-# crumb :project do |project|
-#   link project.name, project_path(project)
-#   parent :projects
-# end
 
-# crumb :project_issues do |project|
-#   link "Issues", project_issues_path(project)
-#   parent :project, project
-# end
-
-# crumb :issue do |issue|
-#   link issue.title, issue_path(issue)
-#   parent :project_issues, issue.project
-# end
-
-# If you want to split your breadcrumbs configuration over multiple files, you
-# can create a folder named `config/breadcrumbs` and put your configuration
-# files there. All *.rb files (e.g. `frontend.rb` or `products.rb`) in that
-# folder are loaded and reloaded automatically when you change them, just like
-# this file (`config/breadcrumbs.rb`).
+crumb :credit do
+  link "支払い方法"
+  parent :user
+end
