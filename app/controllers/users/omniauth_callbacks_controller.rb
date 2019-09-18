ae03f5fee72489413f6c54fd78661ef276cf8e7b
ratovia <ratovia@hotmail.co.jp>
2019-09-18 11:06:42 +0900

    step6

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,27 +3,18 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   ### rails g devise:controllers
 
   def google_oauth2
-    #
-    # 53bmercari　
-    # 
-    # @user = User.from_omniauth(request.env['omniauth.auth'])
-    # if @user.persisted?
-    #   sign_in_and_redirect @user, event: :authentication
-    # else
-    #   render "devise/registrations/new"
-    # end
-
-    #
-    # gem manual https://github.com/zquestz/omniauth-google-oauth2
-    #
-    # @user = User.from_omniauth(request.env['omniauth.auth'])
-    # if @user.persisted?
-    #   flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Google'
-    #   sign_in_and_redirect @user, event: :authentication
-    # else
-    #   session['devise.google_data'] = request.env['omniauth.auth'].except(:extra) # Removing extra as it can overflow some session stores
-    #   redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
-    # end
+    ### mercari-study-step6 
+    ### add google callback
+    ### gem manual https://github.com/zquestz/omniauth-google-oauth2
+    @user = User.from_omniauth(request.env['omniauth.auth'])
+    if @user.persisted?
+      # flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Google'
+      sign_in_and_redirect @user, event: :authentication
+    else
+      # session['devise.google_data'] = request.env['omniauth.auth'].except(:extra) # Removing extra as it can overflow some session stores
+      # redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
+      render "devise/registrations/new"
+    end
   end
   def facebook
     # @user = User.from_omniauth(request.env['omniauth.auth'])
