d24bb1e8632b487d31393c6e0a69f931134b3b3c
ratovia <ratovia@hotmail.co.jp>
2019-09-18 11:12:29 +0900

    step7

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,20 +10,22 @@ class User < ApplicationRecord
   has_many :credits
   has_many :buy_histories
   has_many :items
-  #SNS認証で得られたユーザー情報を取得する
-  # def self.from_omniauth(access_token)
-  #   data = access_token.info
-  #   #emailが既に登録されているか確認する
-  #   user = User.where(email: data['email']).first
-  #   password = Devise.friendly_token[0,20]
-  #   unless user
-  #     user = User.new(
-  #       nickname: data['name'],
-  #       email: data['email'],
-  #       password: password,
-  #       password_confirmation: password
-  #     )
-  #   end
-  #   user
-  # end
+
+  ### mercari-study-7
+  ### add create user from omniauth
+  ### omniauthから取得した認証情報でユーザを作成する。
+  def self.from_omniauth(access_token)
+    data = access_token.info
+    user = User.where(email: data['email']).first
+    password = Devise.friendly_token[0,20]
+    unless user
+        user = User.new(nickname: data['name'],
+          email: data['email'],
+          # password: Devise.friendly_token[0,20]
+          password: password,
+          password_confirmation: password
+        )
+    end
+    user
+  end
 end
