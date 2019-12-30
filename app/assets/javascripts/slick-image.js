5301eb2b8bcddfed85cd161c6394df3008caf33e
ratovia <ratovia@hotmail.co.jp>
2019-12-30 18:40:44 +0900

    add slick

---
+++ app/assets/javascripts/slick-image.js
@@ -0,0 +1,12 @@
+$(function(){
+  $('.top_page_info_area').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
+})
+
+  
