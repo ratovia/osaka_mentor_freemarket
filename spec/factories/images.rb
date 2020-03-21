a1dd2383559240ed860b0d138b62ed865186c38a
Kazushi Yamanishi <keironia187149@gmail.com>
2020-03-21 15:55:19 +0900

    add factories

--- spec/factories/images.rb
+++ spec/factories/images.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
   factory :image do
-    image { File.open("#{Rails.root}/public/images/test_image.jpg") }
+    src { File.open("#{Rails.root}/spec/fixtures/test_image.jpg") }
   end
 end
