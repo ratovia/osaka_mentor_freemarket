7476754a934230e6174f0f5f0555d4bfb9e514f7
Kazushi Yamanishi <keironia187149@gmail.com>
2020-03-21 20:36:59 +0900

    fix image model

--- db/schema.rb
+++ db/schema.rb
@@ -67,8 +67,10 @@ ActiveRecord::Schema.define(version: 2020_03_19_125144) do
 
   create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "src", null: false
+    t.bigint "product_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_images_on_product_id"
   end
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -177,6 +179,7 @@ ActiveRecord::Schema.define(version: 2020_03_19_125144) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "categories", "categories"
+  add_foreign_key "images", "products"
   add_foreign_key "items", "users"
   add_foreign_key "product_categories", "size_groups"
   add_foreign_key "products", "product_categories"
